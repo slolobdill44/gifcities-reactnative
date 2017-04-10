622d9d928af0f351aa49d565c0beebad50cb0f62
Adrian Lobdill <slolobdill44@gmail.com>
2017-04-10 14:19:12 -0700

    Styles search bar

--- header.js
+++ header.js
@@ -1,13 +1,52 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: ""
+    }
+
+    this.submitSearch = this.submitSearch.bind(this);
+  }
+
+  submitSearch() {
+
+  }
+
   render() {
     return (
-      <h2>This is the header where the search bar goes.</h2>
-      <View />
+      <View style={styles.header}>
+        <TextInput
+          placeholder='Search all gifs...'
+          returnKeyType='done'
+          style={styles.input}
+          underlineColorAndroid={'transparent'}/>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    marginTop: 8,
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  input: {
+    paddingLeft: 18,
+    paddingRight: 18,
+    fontSize: 18,
+    fontFamily: "serif",
+    borderWidth: 3,
+    borderColor: 'black',
+    flex: 1,
+    height: 50
+  }
+})
 
 export default Header;
